fdabdafb7d503125b830ce4b237f0f652379fb58
mohitoza08 <mohitoza4420@gmail.com>
2026-05-08 10:43:09 +0530

    add all Adsterra ad units

--- components/AdBanner.js
+++ components/AdBanner.js
@@ -1,3 +1,12 @@
 export default function AdBanner() {
-  return null
+  return (
+    <>
+      <div id="container-d94723c8dc5d3cac2a78c8e9a5c26754" />
+      <script
+        async="async"
+        data-cfasync="false"
+        src="https://pl29373889.profitablecpmratenetwork.com/d94723c8dc5d3cac2a78c8e9a5c26754/invoke.js"
+      />
+    </>
+  )
 }
--- components/AdPopunder.js
+++ components/AdPopunder.js
@@ -2,10 +2,16 @@ import { useEffect } from 'react'
 
 export default function AdPopunder() {
   useEffect(() => {
-    const script = document.createElement('script')
-    script.src = 'https://pl29373823.profitablecpmratenetwork.com/de/4b/6c/de4b6c007b52589d664b5fe825cba77c.js'
-    script.async = true
-    document.body.appendChild(script)
+    const scripts = [
+      'https://pl29373823.profitablecpmratenetwork.com/de/4b/6c/de4b6c007b52589d664b5fe825cba77c.js',
+      'https://pl29373888.profitablecpmratenetwork.com/cc/87/41/cc874143100f3b00c319bf6c137e1ad3.js'
+    ]
+    scripts.forEach(src => {
+      const s = document.createElement('script')
+      s.src = src
+      s.async = true
+      document.body.appendChild(s)
+    })
   }, [])
 
   return null
