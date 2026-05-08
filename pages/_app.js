54b70a0ad118717d410cc59a5fe3cf6a1e661663
mohitoza08 <mohitoza4420@gmail.com>
2026-05-08 12:00:11 +0530

    add Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,15 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <meta name="monetag" content="b7f978c49a859491c0454ec86fd5246a" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-7XYKMQ7D34" />
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-7XYKMQ7D34');
+          `
+        }} />
       </Head>
       <Component {...pageProps} />
     </>
