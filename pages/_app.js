e5a224910d4be5327383ca60fe3607008a70eb1a
mohitoza08 <mohitoza4420@gmail.com>
2026-05-08 10:24:55 +0530

    fix: move monetag meta to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="monetag" content="b7f978c49a859491c0454ec86fd5246a" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
--- pages/_document.js
+++ pages/_document.js
@@ -3,9 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <meta name="monetag" content="b7f978c49a859491c0454ec86fd5246a" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
