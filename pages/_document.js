2381573de862f793ed2daf41eee83967392f094a
mohitoza08 <mohitoza4420@gmail.com>
2026-05-08 10:18:46 +0530

    add monetag meta tag

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="monetag" content="b7f978c49a859491c0454ec86fd5246a" />
+      </Head>
       <body>
         <Main />
         <NextScript />
