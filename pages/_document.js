bd80379e058556e25074db42e17930aca9bc0f64
mohitoza08 <mohitoza4420@gmail.com>
2026-05-08 13:13:55 +0530

    fix preloader image

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
       <body>
         <div id="preloader" className="preloader">
           <div className="preloader-inner">
-            <div className="preloader-logo"><img src="https://res.cloudinary.com/dm2hjn5wp/image/upload/q_auto/f_auto/v1778225949/WhatsApp_Image_2026-05-08_at_13.08.27_gvmasr.jpg" alt="Syco Configs" style="width:80px;height:80px;border-radius:50%;object-fit:cover;border:3px solid rgba(233,69,96,0.3)" /></div>
+            <div className="preloader-logo"><img src="https://res.cloudinary.com/dm2hjn5wp/image/upload/q_auto/f_auto/v1778225949/WhatsApp_Image_2026-05-08_at_13.08.27_gvmasr.jpg" alt="Syco Configs" className="preloader-img" /></div>
             <div className="preloader-text">SYCO CONFIGS</div>
             <div className="preloader-bar"><div className="preloader-fill" /></div>
           </div>
